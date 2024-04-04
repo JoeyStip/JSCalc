7b1094b6b0827af56e41650722ea9827b31a3d21
Joey Stipich <stipichj@gmail.com>
2024-04-04 14:04:23 -0400

    technical debt and finalfinal style adjustments

--- Script.js
+++ Script.js
@@ -1,21 +1,17 @@
-
-let LastPressed = ""
-let currentPressID = ""
 let LastPressedID =  ""
-let parentID = ""
-let Topdisplay = ""
-let Botdisplay = ""
+let display = ""
+let subDisplay = ""
 
 $("button").click(function(){  
-    LastPressed = $(this).text()
-    currentPressID = $(this).attr('id')
-    parentID = $(this).parent().attr('id')
+    let LastPressed = $(this).text()
+    let currentPressID = $(this).attr('id')
+    let parentID = $(this).parent().attr('id')
     
     function ChangeOperator(){
-        if (/\s\W\s\s\W\s$/.test(Botdisplay)){
-            Botdisplay = Botdisplay.replace(/\s\W\s\s\W\s$/,"")
+        if (/(\s\W\s){2}$/.test(subDisplay)){
+            subDisplay = subDisplay.replace(/(\s\W\s){2}$/,"")
         } else {
-            Botdisplay = Botdisplay.slice(0, Topdisplay.length-3)
+            subDisplay = subDisplay.replace(/\s\W\s/, "")
         }
     }
     
@@ -25,75 +21,76 @@ $("button").click(function(){
                 case LastPressedID:
                     break
                 case "clear":
-                    Topdisplay = "0"
-                    Botdisplay = "0"
+                    display = "0"
+                    subDisplay = "0"
                     LastPressed = ""
                     break
                 case "divide":
-                    if(/\W\s$/.test(Botdisplay)){
+                    if(/\W\s$/.test(subDisplay)){
                         ChangeOperator();
                     } 
-                    if(/=/.test(Botdisplay)){
-                        Botdisplay = Botdisplay.match(/(?=\=).+$/g)[0].slice(2)
+                    if(/=/.test(subDisplay)){
+                        subDisplay = subDisplay.match(/(?=\=).+$/g)[0].slice(2)
                     }  
-                    Topdisplay = ""
-                    Botdisplay += " / "
+                    display = ""
+                    subDisplay += " / "
                     break
                 case "multiply":
-                    if(/\W\s$/.test(Botdisplay)){
+                    if(/\W\s$/.test(subDisplay)){
                         ChangeOperator();
                     }
-                    if(/=/.test(Botdisplay)){
-                        Botdisplay = Botdisplay.match(/(?=\=).+$/g)[0].slice(2)
+                    if(/=/.test(subDisplay)){
+                        subDisplay = subDisplay.match(/(?=\=).+$/g)[0].slice(2)
                     } 
-                    Topdisplay = ""
-                    Botdisplay += " * "
+                    display = ""
+                    subDisplay += " * "
                     break
                 case "subtract":
-                    if(/=/.test(Botdisplay)){
-                        Botdisplay = Botdisplay.match(/(?=\=).+$/g)[0].slice(2)
+                    if(/=/.test(subDisplay)){
+                        subDisplay = subDisplay.match(/(?=\=).+$/g)[0].slice(2)
                     } 
-                    Topdisplay = ""
-                    Botdisplay += " - "
+                    display = ""
+                    subDisplay += " - "
                     break
                 case "add":
-                    if(/\W\s$/.test(Botdisplay)){
+                    if(/\W\s$/.test(subDisplay)){
                         ChangeOperator();
                     }
-                    if(/=/.test(Botdisplay)){
-                        Botdisplay = Botdisplay.match(/(?=\=).+$/g)[0].slice(2)
+                    if(/=/.test(subDisplay)){
+                        subDisplay = subDisplay.match(/(?=\=).+$/g)[0].slice(2)
                     } 
-                    Topdisplay = ""
-                    Botdisplay += " + "
+                    display = ""
+                    subDisplay += " + "
                     break
                 case "equals":
-                    Topdisplay = Math.round(eval(Botdisplay)*10000)/10000  
-                    Botdisplay = Botdisplay + " = " + Topdisplay
+                    display = Math.round(eval(subDisplay)*10000)/10000  
+                    subDisplay = subDisplay + " = " + display
                     break
                 }
             break
         case "Numbers": 
-            if(Topdisplay.toString().indexOf("0")==0){ //replaces the zero display with your button press
-                Topdisplay = ""
-                Botdisplay = ""
+            //replaces initial zero display with your button press
+            if(/^0/.test(display)){
+                display = ""
+                subDisplay = ""
             }
-            if(Topdisplay.toString().indexOf(".")<0){//if no decimal, proceed
-                    Topdisplay += LastPressed
-                    Botdisplay += LastPressed
-            }else if($(this).text()=="."){//prevents more than one decimals being entered
+            //makes sure only one decimal present in display 
+            if(LastPressed=="."){
+                if(!/\./.test(display)){//if no decimal, proceed
+                    display += LastPressed
+                    subDisplay += LastPressed
+                }
             } else {
-                Topdisplay += LastPressed
-                Botdisplay += LastPressed
+                display += LastPressed
+                subDisplay += LastPressed
             }
             break
     }
 
     LastPressedID = $(this).attr('id')
     
-    function addCommas(n, x){
-
+    function addCommas(n){
         let parts = n.toString().split(" ")
-
         let callBack=(x)=>{
             if(/\./.test(x)){
                 return x.toString().replace(/\B(?=(\d{3})+(?=\.))/g,",")
@@ -101,7 +98,6 @@ $("button").click(function(){
                 return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")
             }
         }
-
         if(parts.length>1){
             return parts.map(callBack).join(" ")
         } else {
@@ -109,6 +105,6 @@ $("button").click(function(){
         }
     }
 
-    $("#display").text(addCommas(Topdisplay, 0));
-    $("#secondLine").text(addCommas(Botdisplay, 1));
+    $("#display").text(addCommas(display));
+    $("#subDisplay").text(addCommas(subDisplay));
 })
